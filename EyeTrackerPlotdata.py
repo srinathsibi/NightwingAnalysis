830394f637329bda985907262cab3203db251c57
srinathsibi <sibisrinath@gmail.com>
2018-10-31 13:07:42 -0400

    Reading Eyetracker files and beginning function to cleanup eyetracker export

--- EyeTrackerPlotdata.py
+++ EyeTrackerPlotdata.py
@@ -1,2 +1,26 @@
 # Srinath Sibi (ssibi@stanford.edu)
 #Purpose: To analyze Eyetracker Data
+import glob, os, csv
+import matplotlib as plt
+import numpy as np
+
+
+
+
+###############Starting Main Function######################
+if __name__ == '__main__':
+#Look for eye tracking file in Data folder
+    os.chdir('Data/EyeTrackingData/')
+    print "In current folder!"
+    #Listing all files in folder
+    files = os.listdir('.')
+    print "\nAll files in current folder:\n",files
+    for f in files:
+        try:
+            file  = open(f)
+            print "\nOperating on File:\n" , f
+            csv_file = csv.reader(file)
+            #function to process the files
+            file.close()
+        except:
+            sys.exit("Unknown Error Encountered!")
