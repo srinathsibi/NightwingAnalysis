7d35b844935a6877ea3d60874eed7d28a1e25ab3
srinathsibi <sibisrinath@gmail.com>
2018-11-01 14:04:46 -0400

    Identified the participant number successfully

--- EyeTrackerPlotdata.py
+++ EyeTrackerPlotdata.py
@@ -3,17 +3,28 @@
 import glob, os, csv, sys
 import matplotlib as plt
 import numpy as np
-#Line of intro block end is at 31 using enumerate function on rows
-#There are empty lines in the eye tracker export file. Check README.md for
-#more information.
+
 def FileCleaner(filereader):
+    global PARTICIPANTNUMBER
     for i,row in enumerate(filereader):
-        try:
-            if row[0].split('\t')[0] == '##':
-                print "\nEnd of Intro Block at line: ", i, "\n"
-                print row[0].split('\t')[0], " : ", type(row[0].split('\t')[0]), "\n"
+        #this part was written to identify the end of the intro block.
+        #Line of intro block end is at 31 using enumerate function on rows
+        #There are empty lines in the eye tracker export file. Check README.md
+        #try:
+            #if row[0].split('\t')[0] == '##':
+                #print "\nEnd of Intro Block at line: ", i, "\n"
+                #row[0].split('\t') is the list of all elements in a row for the relevant Data
+                #For the intro block , things are still messed up
+                #print row[0].split('\t')[0], " : ", type(row[0].split('\t')[0]), "\n"
+            if (row[0].split('\t')[0]).split(' ')[1] == 'Subject:':
+                    print "\nThis is participant number:\t", row[0].split('\t')[1]
+                    PARTICIPANTNUMBER = float(row[0].split('\t')[2])
         except:
-            print "Error at line :" , i , "\n", row , '\n'
+            pass
+            #print "Error at line :" , i , "\n", row , '\n'
+    #First: Order of events: 1. Get participant number 2. Make Sub-Folder for subject information
+    # 3. Write the information into a information file for each participant
+    # 4. Make a text file for all the relevant data by using
 ###############Starting Main Function######################
 if __name__ == '__main__':
 #Look for eye tracking file in Data folder
