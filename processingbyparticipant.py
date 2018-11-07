b30c4e4e83452f4f96e5175606abc8f8957609b7
srinathsibi <sibisrinath@gmail.com>
2018-11-07 03:26:55 -0500

    Processing iMotions data and now writing the data to a clipped file between Markers 1 and 4

--- processingbyparticipant.py
+++ processingbyparticipant.py
@@ -8,14 +8,22 @@
 #but time stamp formats are all different
 #Data folder only contains the participant folder with sorted data in them. NOTHING ELSE!
 #STEPS: #1. Process the iMotions Data. It has some of the sim data, the physio data and the marker information. It is also quite large, beware!
+#2. The time for the iMotions file is not UTC, it is coded from standadrd military clock. It is imoprtant to note that event though the file says UTC
+#timestamp, it is not!
 import glob, os, csv, sys
 import matplotlib as plt
 import numpy as np
 #Function to process iMotions data
 def ProcessiMoData():
+    iMotionsMarker1TimeAbs =0.00#Abs Marker 1 time for iMotionsData
+    iMotionsMarker1Time =0.00#Marker 1 time for iMotionsData
     fileinfo =[]#Top level file information. To be written
-    iMotionsfile = open(glob.glob('*_*.txt')[0])#There is no other discrening feature to the name of the iMotions file
+    time_abs = []#Calculating absolute time for iMotions Data
+    iMotionsfile = open(glob.glob('P0??.txt')[0])#There is no other discrening feature to the name of the iMotions file
     iMotionsReader = csv.reader(iMotionsfile)
+    #Opening a new file for writing. I am labelling the file as "Clipped"
+    outfile = open('iMotionsClipped.txt','w')
+    outwriter = csv.writer(outfile)
     # We need to read and store the first 4 line of the iMotions file in the clipped data folder, discard line 5, since it is empty and keep line 6, since it is the header line
     i = 1
     while i<=5:
@@ -23,6 +31,41 @@ def ProcessiMoData():
         i=i+1
     headerrow = next(iMotionsReader)
     print "\n\n\nTOP LEVEL INFO:\n\n",fileinfo,'\n\n',headerrow
+    #Searching for the time at which simulator marker is 1
+    for i,row in enumerate(iMotionsReader):
+        iMotionsMarker1TimeAbs = iMotionsTimeConverter(float(row[0].split('\t')[9].split('_')[1]))
+        iMotionsMarker1Time = float(row[0].split('\t')[9].split('_')[1])
+        try:
+            if float(row[0].split('\t')[15]) == 1:
+                break
+        except ValueError:
+            pass
+    print "\n\nMarker 1 Time Stamp : " , iMotionsMarker1Time ,"\n\n"
+    iMotionsfile.seek(0)#Get back to top of the file again
+    #Skipping the first 6 lines
+    i = 1
+    while i<=6:
+        next(iMotionsReader)
+        i=i+1
+    #Reading First Row and writing it manually.
+    #firstrow = next(iMotionsReader)#Reading the first row to get the time start and get the first time stamp
+    #time_abs.append(0.00)
+    #firsttimestamp = iMotionsTimeConverter(float(firstrow[0].split('\t')[9].split('_')[1]))
+    #print "\n\nFirst Time Stamp : " , firsttimestamp ,"\n\n"
+
+    #for row in iMotionsReader:
+    #    print [row[0].split('\t')[9].split('_')[1] for row in filereader]#This is the time for the iMotions file.
+#Function to convert the time read into number of seconds from the start
+def iMotionsTimeConverter(inputcode):
+    decimal = float(inputcode%1000)/1000
+    inputcode = (inputcode - 1000*decimal)/1000
+    secs = float(inputcode%100)
+    inputcode = (inputcode -secs)/100
+    mins = float(inputcode%100)
+    inputcode = (inputcode-mins)/100
+    hours = float(inputcode)
+    time_abs = hours*3600 + mins*60 + secs + decimal
+    return time_abs
 #in the main function
 if __name__=='__main__':
     os.chdir('Data/')#Moving to the data folder
@@ -32,6 +75,6 @@ if __name__=='__main__':
     #The os.listdir() returns a list of strings. each folder name is convenienetly a string
 for foldername in listoffolders:
     os.chdir(foldername+'/')#Navigating into each folder
-    print "Inside the participant data folder : ",foldername,'\n'
+    print "\n\n\nInside the participant data folder : ",foldername,'\n'
     ProcessiMoData()#Function to process the iMotions Data
     os.chdir('../')#Navigating back into the Data folder
