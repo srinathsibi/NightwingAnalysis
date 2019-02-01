7e40592c1ea76a3d1bfb0938db30ec106722f2e9
srinathsibi <you@example.com>
2019-02-01 16:06:03 -0500

    Working with two new files for the signal processing and window separating. The SignalProcessingTest.py is a test file for the creation and testing of the SP for a single file with noisy but good data.

---
+++ SeparateData.py
@@ -0,0 +1,4 @@
+#Author : Srinath Sibi ssibi@stanford.edu
+#Purpose : This code is to create the windows of data with fixed window size and step size.
+#Each window is created with signal processed data. The signal processing is incorporated before
+#the creation of the folder for the respective window size and subfolders for each time step
---
+++ SignalProcessingTest.py
@@ -0,0 +1,3 @@
+#Author : Srinath Sibi ssibi@stanford.edu
+#Purpose : This is a test program to identfy one problematic(noisy) stream of data from one participant
+#and try to band pass filter 
