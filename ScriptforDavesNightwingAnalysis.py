1fa0661051e5429dae73b3de4b05b5aa80d553f7
srinathsibi <you@example.com>
2019-02-20 16:06:39 -0800

    Located the Marker 3 spot, now moving on to clipping data and videos into EndSectionData Folder.

--- ScriptforDavesNightwingAnalysis.py
+++ ScriptforDavesNightwingAnalysis.py
@@ -10,8 +10,59 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 import numpy as np
 def ExtractData(foldername):
-    print "In only function for study\n"
-    iMotionsFile = open()
+    print "In Clipped data folder for:",foldername,"\n"
+    FirstLineArray =[]#First Line Array contains the three first lines from the iMotions, eye tracking and Sim file
+    IOErrorFlag = []#Flag for whether the files are present or not.
+    try:
+        iMotionsFile = open('StrippediMotionsData.csv','r')
+        iMotionsfilereader = csv.reader(iMotionsFile)
+        FirstLineArray.append(next(iMotionsfilereader))
+        iMotionsdata = list(iMotionsfilereader)
+        IOErrorFlag.append(1)
+    except IOError:
+        print "No iMotions file here"
+        IOErrorFlag.append(0)
+    try:
+        EyetrackingFile = open('StrippedEyeTrackingFile.csv','r')
+        Eyetrackingreader = csv.reader(EyetrackingFile)
+        FirstLineArray.append(next(Eyetrackingreader))
+        Eyetrackingdata = list(Eyetrackingreader)
+        IOErrorFlag.append(1)
+    except IOError:
+        print "No EyeTracker file here"
+        IOErrorFlag.append(0)
+    try:
+        Simfile = open('StrippedSimData.csv','r')
+        Simreader = csv.reader(Simfile)
+        FirstLineArray.append(next(Simreader))
+        SimData = list(Simreader)
+        IOErrorFlag.append(1)
+    except IOError:
+        print "No Sim Data here"
+        IOErrorFlag.append(0)
+    if os.path.exists("EndSectionData"):
+        print " End Section data folder already exists."
+    else:
+        print "Creating new directory for End Section data"
+        os.makedirs("EndSectionData")
+    try:
+        if IOErrorFlag[0]:
+            for i in range(len(iMotionsdata)):
+                if float(iMotionsdata[i][2])==5:
+                    Marker3Index = i
+                    Marker3time = iMotionsdata[i][0]
+                    break
+            print "\n Marker from iMotions ", Marker3time
+        elif IOErrorFlag[2]:
+            for i in range(len(SimData)):
+                if float(SimData[i][21])==5:
+                    Marker3Index = i
+                    Marker3time = SimData[i][0]
+                    break
+            print "\n Marker 3 time is : ", Marker3time
+    except UnboundLocalError:
+        print " Unable to locate Marker 3, We need to ignore this participant data ", foldername
+        return None
 #Function to skip lines in the csv files
 def skiplines(fr, lines):
     #fp is file reader and lines is the number of lines to skip
