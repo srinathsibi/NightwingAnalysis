dd7a5f6b6a983bf2e1ae124c25e06a4a73f502f2
srinathsibi <you@example.com>
2019-03-07 17:41:58 -0500

    Starting the script for creating windows of data. The Script for Dave's analysis is the basis for this code.

--- SeparatingIntoWindows.py
+++ SeparatingIntoWindows.py
@@ -1,3 +1,22 @@
 #Author: Srinath Sibi ssibi@stanford.edu
 #Purpose: To create windowed data like Dave's End Section Creation File, but instead of Multiple Windows, we create 1 baseline window from -180 to 0 and several
 # WINDOWSIZE windows, each window's start time a fixed STEPSIZE ahead of the previous window's start time.
+import glob, os, sys, shutil
+import matplotlib as plt
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import numpy as np
+from moviepy.editor import *
+from StripData import PERCLOS
+#Function to load the data to the
+#Main Function
+if __name__=='__main__':
+    os.chdir('Data/')#Moving to the folder containing the data
+    listoffolders = os.listdir('.')
+    for folder in listoffolders:
+        os.chdir(folder+'/ClippedData/')
+        print " Inside folder : ", ClippedData
+        LoadData(folder)
+        os.chdir('../../')
