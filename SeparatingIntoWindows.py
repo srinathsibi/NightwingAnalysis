99cf365f26bcd4910ea895b51040f5272a8ffb28
srinathsibi <you@example.com>
2019-03-12 16:50:23 -0400

    Testing the windowing algorithm to make sure that it's working. Need a way to make sure that just the section folders that got wriiten are retained. The vestigial ones need to be erased. We also need to move the Baseline, (SECTIONXX), EndSection folders to the external drive for backup.

--- SeparatingIntoWindows.py
+++ SeparatingIntoWindows.py
@@ -12,6 +12,8 @@ from moviepy.editor import *
 from StripData import PERCLOS
 from ScriptforDavesNightwingAnalysis import WriteOutputFile
 #Function to load the data from the Stripped Data List
+WINDOWSIZE = 60;#Width of the windows
+WINDOWSTEP = 60;#Difference between the start points of the windows
 def LoadData(folder):
     print "\n\n\n\nIn Clipped data folder for:",folder
     FirstLineArray =[]#First Line Array contains the three first lines from the iMotions, eye tracking and Sim file
@@ -94,6 +96,14 @@ def LoadData(folder):
                         break
         except UnboundLocalError:
                 print " Unable to locate Marker 21. We need to exclude this participant data ", folder
+    #We have to create the Outputfilelist and the data list on the basis of whether eye tracking data
+    #exists for this participant. Titrate here if needed.
+    if IOErrorFlag[1] == 0:
+        Outputfilelist = [ 'iMotionsFile.csv','SimFile.csv' ]
+        Datalist = [iMotionsdata, SimData]
+    elif IOErrorFlag[1]==1:
+        Outputfilelist = [ 'iMotionsFile.csv','EyetrackingFile.csv', 'SimFile.csv' ]
+        Datalist = [iMotionsdata, Eyetrackingdata, SimData]
     # Start with Windows Separation for Automation Condition
     #Start with Baseline.
     ######## NOTE: All the participants have the same time restrictions for baseline segment, so we don't need to get the
@@ -101,29 +111,71 @@ def LoadData(folder):
         if not os.path.exists('Baseline'):
             print " Creating the Baseline Folder data "
             os.makedirs('Baseline')
-        #We have to create the Outputfilelist and the data list on the basis of whether eye tracking data
-        #exists for this participant. Titrate here if needed.
-        if IOErrorFlag[1] == 0:
-            Outputfilelist = [ 'iMotionsFile.csv','SimFile.csv' ]
-            Datalist = [iMotionsdata, SimData]
-        elif IOErrorFlag[1]==1:
-            Outputfilelist = [ 'iMotionsFile.csv','EyetrackingFile.csv', 'SimFile.csv' ]
-            Datalist = [iMotionsdata, Eyetrackingdata, SimData]
         #Using the same iterative mechanism from the Script fro Dave's Analysis
         for i,filename in enumerate(Outputfilelist):
             WriteOutputFile('Baseline/'+filename,(-180.00),(0.00),Datalist[i],FirstLineArray[i])
-        for i,video in enumerate(glob.glob('*.mp4')):
+        '''for i,video in enumerate(glob.glob('*.mp4')):
             print "Processing :" , video , "\n"
             clip = VideoFileClip(video).subclip(0.00, 180.00)
-            clip.write_videofile('Baseline/File' + str(i) +'.mp4' , fps = clip.fps , audio_bitrate="1000k")
+            clip.write_videofile('Baseline/File' + str(i) +'.mp4' , fps = clip.fps , audio_bitrate="1000k")'''
+        #Moving auxillary files
+        try:
+            shutil.copy('EyeTrackingInfo.csv','Baseline/EyeTrackingInfo.csv')
+        except:
+            pass
+        try:
+            shutil.copy('iMotionsInfo.csv','Baseline/iMotionsInfo.csv')
+        except:
+            pass
     except Exception as e:
-        print "\n Exception here : ", e
+        print "\n Exception at Baseline : ", e
     ######## NOTE: We can ognore processing the end section data and use the data from Dave's analysis since it already has the relevant data we need
     ######## NOTE: We need only clip the data in the middle.
+    if int(folder[1:4]) <= 61:
+        if Marker21time >=0:
+            wstart = Marker21time#Time of when the window studys
+        else:
+            wstop = 0
+    elif int(folder[1:4])>=62:
+        wstart = 0
+    windex = 0;#Index of the window
+    wstop = wstart + WINDOWSIZE
+    wstartarray = []
+    while wstop <= (Marker5time - 240):#works for both conditions since they both have marker 5
+        wstartarray.append(wstart)
+        try:
+            #First Creating the folder for the section
+            if not os.path.exists('SECTION'+str(windex)):
+                os.makedirs('SECTION'+str(windex))
+            filepath = 'SECTION'+str(windex)+'/'
+            print "\n\nIn Folder Section"+str(windex) 
+            for i,filename in enumerate(Outputfilelist):
+                WriteOutputFile(filepath+filename,wstart,wstop,Datalist[i],FirstLineArray[i])
+            for i,video in enumerate(glob.glob('*.mp4')):
+                print "Processing :" , video , "\n"
+                clip = VideoFileClip(video).subclip(wstart+180,wstop+180)
+                clip.write_videofile(filepath + 'File' + str(i) + '.mp4' , fps = clip.fps , audio_bitrate="1000k")
+            #Moving auxillary files
+            try:
+                shutil.copy('EyeTrackingInfo.csv',filepath+'EyeTrackingInfo.csv')
+            except:
+                pass
+            try:
+                shutil.copy('iMotionsInfo.csv',filepath+'iMotionsInfo.csv')
+            except:
+                pass
+        except Exception as e:
+            print "Exception at Section Creation of : ", windex , " \n Exception : ", e
+            pass
+        #Updating all the variables in the loop
+        windex = windex+1
+        wstart = wstart + WINDOWSTEP
+        wstop = wstart + WINDOWSIZE
+    print "\n Window Start Times : \n", wstartarray, "\n"
 #Main Function
 if __name__=='__main__':
     os.chdir('Data/')#Moving to the folder containing the data
-    listoffolders = os.listdir('.')
+    listoffolders = ['P062']#os.listdir('.')
     for folder in listoffolders:
         os.chdir(folder+'/ClippedData/')
         LoadData(folder)# The windowing algorithms are called inside the Load Data to avoid the troubles with creating global variables
