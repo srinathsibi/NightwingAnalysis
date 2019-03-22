6e43d6b4e0ce02c8231ec8807ce3f4848c0e6e9f
srinathsibi <you@example.com>
2019-03-22 14:28:58 -0400

    Added a script to copy the ClippedData folder in the Participant folder to the external drive. The path might have to be changed when in Windows.

--- BackupFiles.py
+++ BackupFiles.py
@@ -2,4 +2,23 @@
 #Email : ssibi@stanford.edu
 #Purpose : This script is meant to copy files to the external hard drive or any other drive that the user choses.
 import csv, os, shutil
-PATH = '/media/srinathsibi/Seagate Backup Plus Drive/'
+PATH = '/media/srinathsibi/Seagate Backup Plus Drive/ProcessedData/'
+#Function to check if the folder already exists in the External Drive ProcessedData Folder
+def checkandcopy(folder):
+    print "Checking for folder : ", folder
+    if not os.path.exists(PATH+folder):
+        #os.makedirs(PATH+folder)#Create the folder for the first time
+        shutil.copytree(folder+'/ClippedData',PATH+folder)#Copying the Data from the processing folder
+    else:
+        shutil.rmtree(PATH+folder)
+        shutil.copytree(folder+'/ClippedData',PATH+folder)#Copying the Data from the processing folder
+#Main Function
+if __name__=='__main__':
+    print " Copying ClippedData Folders to the external Drive"
+    #NOTE: The ClippedData Folder in each participant will be copied to the External Drive under the ProcessedData folder.
+    #Shifting to the Data Folder
+    os.chdir('Data/')
+    listoffolders = os.listdir('.')
+    for folder in listoffolders:
+        if int(folder[1:4]) <= 100 and int(folder[1:4])>=0:#Way to curtail the folders that are copied
+            checkandcopy(folder)
